9774871182d7d1cdb43582b795a213f6ad338f1c
Ba1enciaga <108402769+Ba1enciaga@users.noreply.github.com>
2024-12-01 20:27:32 +0900

    Add files via upload
    onClick을 활용한 클릭 기능 실습

--- App.js
+++ App.js
@@ -6,19 +6,22 @@ function App() {
 
   let post = '난곡 라면 맛집'; // post라는 변수를 지정(바인딩 개념)
   let [글제목 , b] = useState(['여자코트추천','남자코트추천','공용니트추천']);
+  let [따봉 , 따봉변경] = useState(0);
 
+  
     // let [a,c]= [1,2]; Destructuring 문법
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉 } </h4>
+        {/* '따봉' state와 onClick을 활용하여 이모티콘 클릭 시 따봉숫자가 1늘어남 */}
       </div>
-      <div className="list">
+      {/* <div className="list">
         <h4>{ 글제목[1] }</h4>
       </div>
       <div className="list">
         <h4>{ 글제목[2] }</h4>
-      </div>
+      </div> */}
     </div>
   );
 }
